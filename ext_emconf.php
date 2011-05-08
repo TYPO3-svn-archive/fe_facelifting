3680f0b903d998c16abeaa21882f0fc152c56753
karlchen321 <karlchen321@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-08 20:39:36 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fe_facelifting/trunk@47441 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fe_facelifting".
 #
-# Auto generated 03-02-2011 23:10
+# Auto generated 08-05-2011 22:15
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'FE_facelifting (using XCLASS)',
-	'description' => 'Change path to Frontend Editing icons  and change width and height of Frontend Editiing PopUp Windows',
+	'description' => 'Change path to Frontend Editing icons and change width and height of feedit PopUps',
 	'category' => 'fe',
 	'shy' => 0,
 	'version' => '0.9.2',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"cbaa";s:10:"README.txt";s:4:"9b98";s:26:"class.ux_tslib_content.php";s:4:"2d5c";s:21:"ext_conf_template.txt";s:4:"0a35";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8800";s:14:"doc/manual.sxw";s:4:"c992";s:41:"view/class.tx_fefacelifting_editpanel.php";s:4:"f24c";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"6402";s:10:"README.txt";s:4:"9b98";s:26:"class.ux_tslib_content.php";s:4:"f5a5";s:21:"ext_conf_template.txt";s:4:"342e";s:12:"ext_icon.gif";s:4:"ac46";s:17:"ext_localconf.php";s:4:"8800";s:13:"locallang.xml";s:4:"da9e";s:14:"doc/manual.sxw";s:4:"4fe2";s:41:"view/class.tx_fefacelifting_editpanel.php";s:4:"1940";}',
+	'suggests' => array(
+	),
 );
 
 ?>
